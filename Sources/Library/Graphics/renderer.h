9b2ed5b573108d58add09434737bf032e2c653f7
Felix Ungman <felix@ungman.com>
2014-01-21 13:17:15 +0100

    Refactor uniforms

--- Sources/Library/Graphics/renderer.h
+++ Sources/Library/Graphics/renderer.h
@@ -14,8 +14,6 @@ extern void CHECK_ERROR_GL();
 #endif
 
 
-
-
 enum shader_uniform_type
 {
 	shader_uniform_type_int,
@@ -48,68 +46,6 @@ inline shader_uniform_type get_shader_uniform_type(const glm::mat2x2*) { return 
 inline shader_uniform_type get_shader_uniform_type(const glm::mat3x3*) { return shader_uniform_type_matrix3; }
 inline shader_uniform_type get_shader_uniform_type(const glm::mat4x4*) { return shader_uniform_type_matrix4; }
 inline shader_uniform_type get_shader_uniform_type(const texture* const*) { return shader_uniform_type_texture; }
-
-
-
-struct plain_uniforms
-{
-	glm::mat4x4 _transform;
-};
-
-
-struct gradient_uniforms
-{
-	glm::mat4x4 _transform;
-	GLfloat _point_size;
-
-	gradient_uniforms() : _point_size(1) {}
-};
-
-
-struct color_uniforms
-{
-	glm::mat4x4 _transform;
-	GLfloat _point_size;
-	glm::vec4 _color;
-
-	color_uniforms() : _point_size(1) {}
-};
-
-
-struct texture_uniforms
-{
-	glm::mat4x4 _transform;
-	const texture* _texture;
-};
-
-struct texture_alpha_uniforms
-{
-	glm::mat4x4 _transform;
-	const texture* _texture;
-	float _alpha;
-};
-
-struct string_uniforms
-{
-	glm::mat4x4 _transform;
-	const texture* _texture;
-	glm::vec4 _color;
-};
-
-
-struct ground_uniforms
-{
-	glm::mat4x4 _transform;
-	const texture* _texture;
-	glm::vec2 _obstacle1;
-	glm::vec2 _obstacle2;
-	glm::vec2 _obstacle3;
-	glm::vec2 _obstacle4;
-	glm::vec2 _obstacle5;
-	glm::vec2 _obstacle6;
-};
-
-
 
 
 inline GLint get_vertex_attribute_size(float*) { return 1; }
