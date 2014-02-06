ea64d8eab03fded1647f1241e0c4f48b27da56db
Felix Ungman <felix@ungman.com>
2014-02-06 09:51:18 +0100

    Add textureshape

---
+++ Sources/Library/Graphics/textureshape.cpp
@@ -0,0 +1,126 @@
+// Copyright (C) 2013 Felix Ungman
+//
+// This file is part of the openwar platform (GPL v3 or later), see LICENSE.txt
+
+#include "textureshape.h"
+
+
+
+texturetile::texturetile() :
+outer(0, 0, 1, 1),
+inner(0, 0, 1, 1)
+{
+}
+
+
+texturetile::texturetile(bounds2f o, bounds2f i) :
+outer(o),
+inner(i)
+{
+}
+
+
+textureatlas::textureatlas(int size_u, int size_v) :
+size(size_u, size_v)
+{
+}
+
+
+texturetile textureatlas::tile(int u0, int v0, int size_u, int size_v, int inset_u, int inset_v)
+{
+	bounds2f outer = bounds2f(u0, v0, size_u, size_v) / glm::vec2(size.x, size.y);
+	bounds2f inner = outer.grow(-(float)inset_u / size.x, -(float)inset_v / size.y);
+	return texturetile(outer, inner);
+}
+
+
+textureglyph::textureglyph(texturetile tile, bounds2f bounds, glm::vec2 inset)
+{
+	outer_xy = bounds;
+	inner_xy = bounds.grow(-inset.x, -inset.y);
+	outer_uv = tile.outer;
+	inner_uv = tile.inner;
+}
+
+
+vertexglyph2<glm::vec2, glm::vec2> textureglyph::glyph()
+{
+	return vertexglyph2<glm::vec2, glm::vec2>([this](std::vector<vertex_type>& vertices) {
+		generate(vertices);
+	});
+}
+
+
+void textureglyph::generate(std::vector<vertex_type>& vertices)
+{
+	bool min_x = outer_xy.min.x < inner_xy.min.x;
+	bool max_x = inner_xy.max.x < outer_xy.max.x;
+	bool min_y = outer_xy.min.y < inner_xy.min.y;
+	bool max_y = inner_xy.max.y < outer_xy.max.y;
+
+	if (min_x && min_y)
+		rectangle(vertices,
+			bounds2f(outer_xy.min.x, outer_xy.min.y, inner_xy.min.x, inner_xy.min.y),
+			bounds2f(outer_uv.min.x, outer_uv.max.y, inner_uv.min.x, inner_uv.max.y));
+
+	if (min_x)
+		rectangle(vertices,
+			bounds2f(outer_xy.min.x, inner_xy.min.y, inner_xy.min.x, inner_xy.max.y),
+			bounds2f(outer_uv.min.x, inner_uv.max.y, inner_uv.min.x, inner_uv.min.y));
+
+	if (min_x && max_y)
+		rectangle(vertices,
+			bounds2f(outer_xy.min.x, inner_xy.max.y, inner_xy.min.x, outer_xy.max.y),
+			bounds2f(outer_uv.min.x, inner_uv.min.y, inner_uv.min.x, outer_uv.min.y));
+
+	if (min_y)
+		rectangle(vertices,
+			bounds2f(inner_xy.min.x, outer_xy.min.y, inner_xy.max.x, inner_xy.min.y),
+			bounds2f(inner_uv.min.x, outer_uv.max.y, inner_uv.max.x, inner_uv.max.y));
+
+	rectangle(vertices,
+		bounds2f(inner_xy.min.x, inner_xy.min.y, inner_xy.max.x, inner_xy.max.y),
+		bounds2f(inner_uv.min.x, inner_uv.max.y, inner_uv.max.x, inner_uv.min.y));
+
+	if (max_y)
+		rectangle(vertices,
+			bounds2f(inner_xy.min.x, inner_xy.max.y, inner_xy.max.x, outer_xy.max.y),
+			bounds2f(inner_uv.min.x, inner_uv.min.y, inner_uv.max.x, outer_uv.min.y));
+
+	if (max_x && min_y)
+		rectangle(vertices,
+			bounds2f(inner_xy.max.x, outer_xy.min.y, outer_xy.max.x, inner_xy.min.y),
+			bounds2f(inner_uv.max.x, outer_uv.max.y, outer_uv.max.x, inner_uv.max.y));
+
+	if (max_x)
+		rectangle(vertices,
+			bounds2f(inner_xy.max.x, inner_xy.min.y, outer_xy.max.x, inner_xy.max.y),
+			bounds2f(inner_uv.max.x, inner_uv.max.y, outer_uv.max.x, inner_uv.min.y));
+
+	if (max_x && max_y)
+		rectangle(vertices,
+			bounds2f(inner_xy.max.x, inner_xy.max.y, outer_xy.max.x, outer_xy.max.y),
+			bounds2f(inner_uv.max.x, inner_uv.min.y, outer_uv.max.x, outer_uv.min.y));
+
+}
+
+
+void textureglyph::rectangle(std::vector<vertex_type>& vertices, bounds2f xy, bounds2f uv)
+{
+	vertices.push_back(vertex_type(glm::vec2(xy.min.x, xy.min.y), glm::vec2(uv.min.x, uv.min.y)));
+	vertices.push_back(vertex_type(glm::vec2(xy.min.x, xy.max.y), glm::vec2(uv.min.x, uv.max.y)));
+	vertices.push_back(vertex_type(glm::vec2(xy.max.x, xy.max.y), glm::vec2(uv.max.x, uv.max.y)));
+	vertices.push_back(vertex_type(glm::vec2(xy.max.x, xy.max.y), glm::vec2(uv.max.x, uv.max.y)));
+	vertices.push_back(vertex_type(glm::vec2(xy.max.x, xy.min.y), glm::vec2(uv.max.x, uv.min.y)));
+	vertices.push_back(vertex_type(glm::vec2(xy.min.x, xy.min.y), glm::vec2(uv.min.x, uv.min.y)));
+}
+
+
+textureshape::textureshape()
+{
+}
+
+
+textureshape::~textureshape()
+{
+}
---
+++ Sources/Library/Graphics/textureshape.h
@@ -0,0 +1,59 @@
+// Copyright (C) 2013 Felix Ungman
+//
+// This file is part of the openwar platform (GPL v3 or later), see LICENSE.txt
+
+#ifndef textureshape_H
+#define textureshape_H
+
+#include "vertexshape.h"
+
+
+struct texturetile
+{
+	bounds2f outer;
+	bounds2f inner;
+
+	texturetile();
+	texturetile(bounds2f o, bounds2f i);
+};
+
+struct textureatlas
+{
+	glm::ivec2 size;
+
+	textureatlas(int size_u, int size_v);
+	texturetile tile(int u0, int v0, int size_u, int size_v, int insert_u, int inset_v);
+};
+
+
+class textureglyph
+{
+public:
+	typedef vertex2<glm::vec2, glm::vec2> vertex_type;
+
+	bounds2f outer_xy;
+	bounds2f inner_xy;
+	bounds2f outer_uv;
+	bounds2f inner_uv;
+
+	textureglyph() { }
+	textureglyph(texturetile tile, bounds2f bounds, glm::vec2 inset);
+
+	vertexglyph2<glm::vec2, glm::vec2> glyph();
+
+	void generate(std::vector<vertex_type>& vertices);
+	void rectangle(std::vector<vertex_type>& vertices, bounds2f xy, bounds2f uv);
+};
+
+
+class textureshape : public vertexshape2<glm::vec2, glm::vec2>
+{
+public:
+	textureshape();
+	~textureshape();
+
+
+};
+
+
+#endif
